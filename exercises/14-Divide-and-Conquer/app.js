438ef0b0ebba38bdc5c35fa808b2349920b6b0d3
José Francisco Carrión Ramos <145589365+JFCR14@users.noreply.github.com>
2024-12-29 20:23:02 +0000

    1.0

--- exercises/14-Divide-and-Conquer/app.js
+++ exercises/14-Divide-and-Conquer/app.js
@@ -1,6 +1,20 @@
 let listOfNumbers = [4, 80, 85, 59, 37, 25, 5, 64, 66, 81, 20, 64, 41, 22, 76, 76, 55, 96, 2, 68];
 
 // Your code here
-functionmargeTwoList(listOfNumbers){
-    
+function mergeTwoList(listOfNumbers){
+    let newArray = [];
+    let odd = [];
+    let even = [];
+    listOfNumbers.forEach(i => {
+        if (i % 2 === 0) {
+            even.push(i);
+        } else {
+            odd.push(i);
+        }
+
+    });
+    newArray = odd.concat(even);
+    return newArray;
 }
+
+console.log(mergeTwoList(listOfNumbers));
--- exercises/15-Max-integer-from-array/app.js
+++ exercises/15-Max-integer-from-array/app.js
@@ -1,3 +1,16 @@
 let myArray = [43,23,6,87,43,1,4,6,3,67,8,3445,3,7,5435,63,346,3,456,734,6,34];
 
 // Your code here
+function findMax(myArray) {
+    let max = 0;
+
+    myArray.forEach(i => {
+        if (i > max) {
+            max = i;
+        }
+    });
+    return max;
+    
+}
+
+console.log(findMax(myArray));
