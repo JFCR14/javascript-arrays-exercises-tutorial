a2f755942ac7e50d6a8905653c9b5802a2f43e6e
José Francisco Carrión Ramos <145589365+JFCR14@users.noreply.github.com>
2024-12-29 20:37:42 +0000

    1.0

--- exercises/17-The-for-loop/app.js
+++ exercises/17-The-for-loop/app.js
@@ -1,3 +1,7 @@
 let myArray = [2323, 4344, 2325, 324413, 21234, 24531, 2123, 42234, 544, 456, 345, 42, 5445, 23, 5656, 423];
-
+let sumaElementos = 0;
 // Your code here
+for (const i of myArray) {
+    sumaElementos = sumaElementos + i;
+}
+console.log(sumaElementos / myArray.length);
