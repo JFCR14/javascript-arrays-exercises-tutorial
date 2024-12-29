95c9c1dee2986d8c1ef621d30d6f0094eb6fa823
José Francisco Carrión Ramos <145589365+JFCR14@users.noreply.github.com>
2024-12-29 19:19:18 +0000

    1.0

--- .learn/resets/06.5-One-last-looping/app.js
+++ .learn/resets/06.5-One-last-looping/app.js
@@ -1,7 +1,3 @@
 let mySampleArray = ['Esmeralda', 'Kiko', 'Ruth', 'Lebron', 'Pedro', 'Maria', 'Lou', 'Fernando', 'Cesco', 'Bart', 'Annie'];
 
 // Your code here
-for (let i = mySampleArray.length-1; i >=0; i--) {
-   let nombre = mySampleArray[i];
-   console.log(nombre);
-}
--- exercises/07.3-Flip-Array/app.js
+++ exercises/07.3-Flip-Array/app.js
@@ -1,3 +1,11 @@
 let arr = [45,67,87,23,5,32,60];
 
 // Your code here
+const arrInvertido = [];
+for (let i = arr.length-1; i >= 0; i--) {
+    
+    let item = arr[i];
+    arrInvertido.push(item);
+}
+
+console.log(arrInvertido);
